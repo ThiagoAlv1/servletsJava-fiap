e78f401fe03fb118d4db5adb98bdcad2a2f37b24
Thiago Alves <thiago___melo@hotmail.com>
2023-09-18 20:50:46 -0300

    concluido

--- src/main/java/servlets/HelloServlet.java
+++ src/main/java/servlets/HelloServlet.java
@@ -21,8 +21,11 @@ public class HelloServlet extends HttpServlet {
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
+
     }
 
     public void destroy() {
     }
+
+
 }
